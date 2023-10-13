b08a913035b5383d7114700e679494fee8ab46f0
akirousnow <ofsanker@qq.com>
2023-10-13 13:44:15 +0800

    bbb

--- app.vue
+++ app.vue
@@ -1,5 +1,5 @@
 <template>
   <div>
-    <h1 class="flex items-center w-screen h-screen justify-center text-4xl text-yellow-600 text-center">你好，世界12356</h1>
+    <h1 class="flex items-center w-screen h-screen justify-center text-4xl text-yellow-600 text-center">你AA好，世界12356</h1>
   </div>
 </template>
